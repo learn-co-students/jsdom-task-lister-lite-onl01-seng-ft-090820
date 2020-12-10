d840cb4ed138cfa90067811925e04f3a54b20572
LeahESc <leahemily1@gmail.com>
2020-12-10 14:35:55 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addEventListeners()
 });
+
+function addEventListeners() {
+  const commentSubmit = document.getElementById("create-task-form")
+  commentSubmit.addEventListener('submit', handleSubmit)
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+  const input = e.target.querySelector("#new-task-description")
+  const inputValue = input.value
+  const li = document.createElement('li')
+  li.textContent = inputValue
+  const task = document.querySelector("#tasks")
+  task.appendChild(li)
+  e.target.querySelector("#new-task-description").value = ""
+}
