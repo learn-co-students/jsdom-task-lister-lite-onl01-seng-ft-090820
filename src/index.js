41cfdca1eb6f3f550a00540ace2f7e8418879202
Ariel_Ibarra <arielibarra39@gmail.com>
2020-12-14 12:26:30 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addEventListener() 
 });
+
+function addEventListener() {
+  const createTaskForm = document.getElementById('create-task-form')
+  createTaskForm.addEventListener('submit', handleSubmit)
+}
+
+function handleSubmit(e) {
+  e.preventDefault()
+  const input = e.target.querySelector("#new-task-description").value
+  const pTag = document.createElement('p')
+  pTag.textContent = input 
+  const tasks = document.querySelector("#tasks")
+  tasks.appendChild(pTag)
+  e.target.querySelector("#new-task-description").value = ""
+}
